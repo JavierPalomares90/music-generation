ee6570f2f6adcfedb5c8c4dbd849b850a2597093
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-14 11:29:01 -0500

    halfway through getting urls recursively

--- midi_scraper.py
+++ midi_scraper.py
@@ -1,6 +1,11 @@
-# Python class to download Midi files from a url
-# Written for python3
-# Usage:
+'''
+ Python class to download Midi files from a url
+ Written for python3
+ Usage: python3 -u <url> -d <save_dir> 
+  Optional arguments:
+   -r, --recursive. Recurse through a url for sublinks and 
+          download all midis in the sublinks
+'''
 import argparse
 import heapq
 import re
@@ -10,14 +15,34 @@ import os
 from os import listdir
 from os.path import isfile, join
 from bs4 import BeautifulSoup
+import errno
+from datetime import datetime
 
-# get all the links that go to midi files
-def get_all_midi_links(url):
+def get_status_code(link):
+    """
+    Return the error code for any url
+    param: link
+    """
+    try:
+        error_code = requests.get(link).status_code
+    except requests.exceptions.ConnectionError:
+        error_code = 500
+    return error_code
+
+def get_soup(url):
+    """
+    Return the BeautifulSoup object for input link
+    """
     # connect to the url
     website = urlopen(url)
     # get the html code
     html = website.read()
     soup = BeautifulSoup(html,features="html.parser")
+    return soup
+
+# get all the links that go to midi files
+def get_all_midi_links(url):
+    soup = get_soup(url)
     links = soup.findAll(href=re.compile(r"\.mid$"))
     return links
 
@@ -40,17 +65,54 @@ def download_midis(url,midis,save_dir):
         f = open(path,'wb+')
         f.write(midi.read())
         f.close()
+        
+
+# find all of the sub_urls in a root url
+def get_all_sub_urls(source_url,depth = 0, max_depth = 5):
+        if depth > max_depth:
+                return {}
+        all_urls_info = []
+        status_dict = {}
+        soup = get_soup(source_url)
+        a_tags = soup.findAll("a", href=True)
+        for a_tag in a_tags:
+            if "http" not in a_tag["href"] and "/" in a_tag["href"]:
+                url = "http://www.lufthansa.com" + a_tag['href']
+            elif "http" in a_tag["href"]:
+                url = a_tag["href"]
+            else:
+                continue
+            status_dict["url"] = url
+            status_dict["status_code"] = get_status_code(url)
+            status_dict["timestamp"] = datetime.now()
+            status_dict["depth"] = depth + 1
+            all_urls_info.append(status_dict)
+        return all_urls_info
+
+
+def get_midis(source_url,save_dir,recursive=False):
+        if (recursive == True):
+                max_depth = 4
+                urls = get_all_sub_urls(source_url,0,max_depth)
+        else:
+                urls = [source_url]
+        for url in urls:
+                midi_links = get_all_midi_links(source_url)
+                download_midis(source_url,midi_links,save_dir)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Download all midi files from a url')
     parser.add_argument('-u','--url',required=True,help="The url to download midis from")
     parser.add_argument('-d','--directory',required=True,help='Where to save the midis to')
+    parser.add_argument('-r','--recursive',help='Recurse through the given url for midis')
     args = vars(parser.parse_args())
     source_url = args["url"]
     save_dir = args["directory"]
-    midi_links = get_all_midi_links(source_url)
-    download_midis(source_url,midi_links,save_dir)
+    recursive = False
+    if(args["recursive"]):
+            recursive = True
+    get_midis(source_url,save_dir,recursive)
 
 if __name__=='__main__':
     main()
