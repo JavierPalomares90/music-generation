3f4f950a0514f753f67eff512df02927b1443167
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-15 22:13:43 -0500

    Testing reading midi files with pymidifile

--- read_midis.py
+++ read_midis.py
@@ -4,17 +4,30 @@
  Usage: python3 -d <midi_file_directory> 
 '''
 import argparse
+import os
+from pymidifile import *
 
-def load_midis(dir):
-    # TODO: complete impl
-    0
+def get_midi_paths(dir):
+    # Find all the midi files in the directory
+    paths = []
+    for root,dirs,files in os.walk(dir):
+        for file in files:
+            if file.endswith(".mid"):
+                paths.append(os.join(root,file))
+    return paths
+
+def get_pandas_dataframes(paths):
+    for path in paths:
+        matrix = mid_to_matrix(path)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Download all midi files from a url')
     parser.add_argument('-d','--directory',required=True,help='Where to save the midis to')
     args = vars(parser.parse_args())
     dir = args["directory"]
-    load_midis(dir)
+    paths = get_midi_paths(dir)
+    get_pandas_dataframes(paths)
 
 
 
