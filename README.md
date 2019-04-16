d8f99d7884cada772cc14fbc3dfdf54280339304
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-15 21:43:26 -0500

    Initial commit of class to read midi files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Music Generation
+Repo for data mining project to generate original music after being trained on a collection of midi files
+
+## To start:
+### Download midi files from a given url
+usage: python3 midi_scraper.py -u <url> -d <save_directory>
---
+++ read_midis.py
@@ -0,0 +1,18 @@
+'''
+ Python class to load the download midi files
+ Written for python3
+ Usage: python3 -d <midi_file_directory> 
+'''
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Download all midi files from a url')
+    parser.add_argument('-d','--directory',required=True,help='Where to save the midis to')
+    args = vars(parser.parse_args())
+    dir = args["directory"]
+    load_midis(dir)
+
+
+
+if __name__=='__main__':
+    main()
