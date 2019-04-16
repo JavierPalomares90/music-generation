6da60a8cce13663a00e2c188ec27acde76ddf98a
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-15 21:44:13 -0500

    Initial commit of class to read midi files

--- read_midis.py
+++ read_midis.py
@@ -5,6 +5,10 @@
 '''
 import argparse
 
+def load_midis(dir):
+    # TODO: complete impl
+    0
+
 def main():
     parser = argparse.ArgumentParser(description='Download all midi files from a url')
     parser.add_argument('-d','--directory',required=True,help='Where to save the midis to')
