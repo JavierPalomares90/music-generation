7255568b04962c4bc4bf7032bf66fac28b1d73c0
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-29 20:32:19 -0500

    add script to remove duplicated files

---
+++ remove_dups.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# python script to transform python files
+
+import argparse
+import os
+import utils
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--data_dir', type=str, default='data/midi',
+                        help='data directory containing .mid files to use for' \
+                             'training')
+    parser.parse_args()
+    args = vars(parser.parse_args())
+    data_dir = args["data_dir"]
+    midi_paths = utils.get_midi_paths(data_dir)
+    for p in midi_paths:
+        try:
+            file_name = os.path.splitext(p)[0]
+            dir_path = os.path.dirname(p)
+            format0_file = file_name + "format0.mid"
+            if os.path.exists(os.path.join(dir_path,format0_file)):
+                os.remove(p)
+        except Exception as e:
+            print("Unable to reformat {}. Deleting it".format(p))
+            os.remove(p)
+
+if __name__ == '__main__':
+    main()
