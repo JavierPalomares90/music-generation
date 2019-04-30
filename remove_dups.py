f0db781458ab19fb7dc1c050c2937b5a55f04b1c
Javier Palomares <javier.palomares.90@gmail.com>
2019-04-29 20:44:09 -0500

    Script used to remove duplicates

--- remove_dups.py
+++ remove_dups.py
@@ -6,14 +6,13 @@ import os
 import utils
 
 def main():
-    #parser = argparse.ArgumentParser(
-    #    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    #parser.add_argument('--data_dir', type=str, default='data/midi',
-    #                    help='data directory containing .mid files to use for' \
-    #                         'training')
-    #parser.parse_args()
-    #args = vars(parser.parse_args())
-    #data_dir = args["data_dir"]
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--data_dir', type=str, default='data/midi',
+                        help='data directory containing .mid files to use for' \
+                             'training')
+    parser.parse_args()
+    args = vars(parser.parse_args())
     data_dir = 'midi_files/reformated'
     midi_paths = utils.get_midi_paths(data_dir)
     for p in midi_paths:
