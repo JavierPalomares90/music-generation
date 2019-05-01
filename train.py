d0974d7a586010471995193018876b1928167cd3
Javier Palomares <javier_palomares@homedepot.com>
2019-05-01 13:52:23 -0500

    fix filepath

--- train.py
+++ train.py
@@ -80,14 +80,14 @@ def get_model(args,experiment_dir=None):
                   metrics=['mean_squared_error'])
     return model, epoch
 
-def get_tf_callbacks(experiment_dir, checkpoint_monitor='val_acc'):
+def get_tf_callbacks(experiment_dir, checkpoint_monitor='val_mean_squarred_error'):
     
     callbacks = []
     
     # save model checkpoints
     filepath = os.path.join(experiment_dir, 
                             'checkpoints', 
-                            'checkpoint-epoch_{epoch:03d}-val_acc_{val_acc:.3f}.hdf5')
+                            'checkpoint-epoch_{epoch:03d}-val_mean_squarred_error{val_mean_squarred_error:.3f}.hdf5')
 
     callbacks.append(ModelCheckpoint(filepath, 
                                      monitor=checkpoint_monitor, 
